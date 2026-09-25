acbec7143f4d760db2a0c9c4a7adf27744465b14
sunay <sunay@jiemogame.com>
2026-09-26 02:24:55 +0800

    Add 2023 HBN award-winning advertising film to portfolio

--- src/content/projects/index.ts
+++ src/content/projects/index.ts
@@ -1,4 +1,5 @@
-﻿import type { Project } from "./types";
+import hbnProject from "./hbn-daguangsai-film/project";
+import type { Project } from "./types";
 import project1 from "./xuanzhi-ai-short-film/project";
 import project2 from "./gentle-monster-folded-dimension/project";
 import project3 from "./future-echoes-visual-extension/project";
@@ -10,6 +11,6 @@ import project9 from "./shanxia-yousong-fragrance-illustration/project";
 import project10 from "./3d-asset-production/project";
 import project11 from "./unity-interactive-scene-study/project";
 
-export const projects: Project[] = [project1, project2, project3, project4, project5, project7, project8, project9, project10, project11];
+export const projects: Project[] = [project1, hbnProject, project2, project3, project4, project5, project7, project8, project9, project10, project11];
 
 export type { PortfolioImage, Project, ProjectBlock, ProjectResource } from "./types";
